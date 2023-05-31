bddc39b436ec268309c2cf01eb3766940c5d62f8
swethasuresh27 <swethasujas@gmail.com>
2023-05-31 10:38:28 +0530

    new project creation

---
+++ src/main/java/SeleniumPractice/SeleniumPractice/PrintCount2.java
@@ -0,0 +1,17 @@
+package SeleniumPractice.SeleniumPractice;
+import java.util.Scanner;
+public class PrintCount2 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan=new Scanner(System.in);
+			
+			String nextLine=scan.nextLine();
+			String[]spilt=nextLine.split(" ");
+			System.out.println(spilt.length);
+
+	}
+
+}
